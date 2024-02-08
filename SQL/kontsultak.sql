f8457673dff9de03d7dbc30f8b8863f8ae9a0562
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-08 13:21:34 +0100

    2 kontsulta eginda

--- SQL/kontsultak.sql
+++ SQL/kontsultak.sql
@@ -4,12 +4,13 @@
 select count(idfilma) ikusita , izenburua  
 from saioa inner join filma using(idfilma)
 group by (izenburua);
+
 #Bezero gutxien dituen zinema, bezero-kopurua adierazita.
 
 #Zineman areto gehien dituzten zineek proiektatutako filmen batez besteko iraupena.
-select zinema.izena, avg(iraupena) #no esta terminado 
-from filma inner join saioa using (idfilma)
-			inner join aretoa using (idaretoa)
-            inner join zinema on aretoa.idzinema = zinema.idzinema;
+
 
 #Film genero bakoitzeko, estadistikak. 
+select generoa, sum(iraupena)/4, count(idsaioa)
+from filma inner join saioa using(idfilma)
+group by generoa
